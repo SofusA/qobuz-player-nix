fa9405d4d9034fd321b340cb56e09717a8b2dec4
Sofus Addington <sofus@addington.dk>
2025-01-31 08:35:17 +0100

    Add flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "A flake to download, extract, and install qobuz-player";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      packages = {
+        qobuz-player = pkgs.stdenv.mkDerivation {
+          name = "qobuz-player";
+          src = pkgs.fetchurl {
+            url = "https://github.com/sofusa/qobuz-player/releases/download/v0.2/qobuz-player-x86_64-unknown-linux-gnu.tar.gz";
+            sha256 = "sha256-OcxKklaVjUwo3R71YtSR9+i8zOOoXJ656+Zvhou5jZU=";
+          };
+
+          unpackPhase = ''
+            tar xf $src
+          '';
+
+          installPhase = ''
+            install -m755 -D qobuz-player $out/bin/qobuz-player
+          '';
+        };
+      };
+    }
+  );
+}
